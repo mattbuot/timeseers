2edaf2264895b37be827da0d37dd1893a15a44a1
Matthieu <matthieu.buotdelepine@odaia.ai>
2022-08-05 12:30:38 +0200

    moved models to the same folder

--- src/timeseers/__init__.py
+++ src/timeseers/__init__.py
@@ -1,11 +1,19 @@
-from timeseers.linear_trend import LinearTrend
-from timeseers.timeseries_model import TimeSeriesModel
-from timeseers.fourier_seasonality import FourierSeasonality
-from timeseers.rbf_seasonality import RBFSeasonality
-from timeseers.logistic_growth import LogisticGrowth
-from timeseers.indicator import Indicator
-from timeseers.constant import Constant
-from timeseers.regressor import Regressor
+from timeseers.models.linear_trend import LinearTrend
+from timeseers.models.timeseries_model import TimeSeriesModel
+from timeseers.models.fourier_seasonality import FourierSeasonality
+from timeseers.models.rbf_seasonality import RBFSeasonality
+from timeseers.models.logistic_growth import LogisticGrowth
+from timeseers.models.indicator import Indicator
+from timeseers.models.constant import Constant
+from timeseers.models.regressor import Regressor
 
-__all__ = ["LinearTrend", "TimeSeriesModel", "FourierSeasonality", "Indicator",
-           "Constant", "Regressor", "LogisticGrowth", "RBFSeasonality"]
+__all__ = [
+    "LinearTrend",
+    "TimeSeriesModel",
+    "FourierSeasonality",
+    "Indicator",
+    "Constant",
+    "Regressor",
+    "LogisticGrowth",
+    "RBFSeasonality",
+]
